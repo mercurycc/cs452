f2a1657fa0f2bf81b87f16fec1be59e41ec3a99b
gzwang <gzwang@uwaterloo.ca>
2011-05-25 22:15:42 -0400

    RPS server implemented

--- dev/kernel/src/kernel.mk
+++ dev/kernel/src/kernel.mk
@@ -32,6 +32,7 @@ KERNELSRC += lib/hashtable.c
 KERNELSRC += userland/apps/init/main.c
 KERNELSRC += userland/apps/init_user/main.c
 KERNELSRC += userland/apps/noise/main.c
+KERNELSRC += userland/apps/RPSServer/main.c
 
 # Libraries
 KERNELSRC += userland/lib/syscall.c
