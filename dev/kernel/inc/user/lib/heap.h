a2f9b0cf49af5c02b691871a68cbc916e7df9851
Wendell-PC <gzwang@uwaterloo.ca>
2011-06-02 18:32:50 -0400

    heap separated

---
+++ dev/kernel/inc/user/lib/heap.h
@@ -0,0 +1,30 @@
+#ifndef _USER_LIB_HEAP_H_
+#define _USER_LIB_HEAP_H_
+
+typedef struct Heap_node_s Heap_node;
+typedef struct Heap_s Heap;
+
+#define MAX_HEAP_SIZE 64
+
+enum HEAP_ERR {
+	ERR_HEAP_NONE,
+	ERR_HEAP_FULL,
+	ERR_HEAP_EMPTY
+};
+
+struct Heap_node_s {
+	uint key;
+	int value;
+};
+
+struct Heap_s {
+	Heap_node data[MAX_HEAP_SIZE];
+	int size;
+};
+
+int heap_init( Heap* heap );
+int heap_insert( Heap* heap, Heap_node* elem );
+int heap_read_top( Heap* heap, Heap_node* elem );	//peak
+int heap_remove_top ( Heap* heap, Heap_node* elem );
+
+#endif
