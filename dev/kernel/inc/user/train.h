4c0fae5449fed0a01cd87800e4b4337913e1dc15
Wendell-PC <gzwang@uwaterloo.ca>
2011-06-12 17:02:38 -0400

    new train file

---
+++ dev/kernel/inc/user/train.h
@@ -0,0 +1,19 @@
+#ifndef _USER_TRAIN_H_
+#define _USER_TRAIN_H_
+
+/* task priorities */
+#define TRAIN_MODULE_PRIORITY 5
+#define TRAIN_CLOCK_PRIORITY 5
+#define TRAIN_SENSOR_PRIORITY 5
+
+/* train event api */
+int train_update_time( int ticks );
+int train_set_speed( int speed );
+int train_reverse();
+int train_switch( int switch_id, int direction );
+int train_last_switch();
+int train_last_sensor();
+int train_all_sensor();
+int train_module_suicide();
+
+#endif
