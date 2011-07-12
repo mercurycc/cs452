51990a59fdefb79d5b0812f184429322f8a96cdf
Ze Long <z2long@uwaterloo.ca>
2011-07-12 18:41:28 -0400

    Progress save: change planner delay back to work around

--- dev/kernel/src/userland/apps/train/planner.c
+++ dev/kernel/src/userland/apps/train/planner.c
@@ -395,7 +395,11 @@ static int train_forward_stop( volatile Train_data* train, Rbuf* path, volatile 
 		train_auto_set_speed( auto_tid, train->id, 0 );
 		dprintf( "Train %d stop\n", train->id );
 
-		Delay( 500 );
+		Delay( 100 );
+		Delay( 100 );
+		Delay( 100 );
+		Delay( 100 );
+		Delay( 100 );
 
 		dprintf( "Train %d forward execution completed\n", train->id );
 	}
