31cda210e396d6593b60ab2e9d2bfd71514e3370
Ze Long <z2long@uwaterloo.ca>
2011-07-18 16:54:19 -0400

    Progress save: planner able to avoid occupied path

--- dev/kernel/src/userland/apps/train/track_reserve.c
+++ dev/kernel/src/userland/apps/train/track_reserve.c
@@ -15,6 +15,7 @@
 
 enum Track_reserve_type {
 	TRACK_RESERVE_GET_RANGE,
+	TRACK_RESERVE_MAY_I,
 	TRACK_RESERVE_GET,
 	TRACK_RESERVE_PUT
 };
@@ -30,7 +31,7 @@ typedef struct Track_reserve_reply_s {
 	int direction;          /* Againist or same side, see track_reserve.h */
 } Track_reply;
 
-int track_reserved( Train* train, track_node* node, int direction )
+static int track_reserved( Train* train, track_node* node, int direction )
 {
 	track_edge* edge;
 	
@@ -102,6 +103,12 @@ void track_reserve()
 			switch_table = request.train->switch_table;
 		}
 
+		/* Convert direction */
+		direction = DIR_AHEAD;
+		if( node->type == NODE_BRANCH && switch_table[ SWID_TO_ARRAYID( node->id + 1 ) ] == 'C' ){
+			direction = DIR_CURVED;
+		}
+
 		switch( request.type ){
 		case TRACK_RESERVE_GET_RANGE:
 			range = request.range_dir;
@@ -121,18 +128,14 @@ void track_reserve()
 				node = track_next_node( node, switch_table );
 			} while( range > 0 );
 			break;
+		case TRACK_RESERVE_MAY_I:
+			direction = request.range_dir;
+			reply.direction = track_reserved( request.train, node, direction );
+			break;
 		case TRACK_RESERVE_GET:
-			direction = DIR_AHEAD;
-			if( node->type == NODE_BRANCH && switch_table[ SWID_TO_ARRAYID( node->id + 1 ) ] == 'C' ){
-				direction = DIR_CURVED;
-			}
 			reply.direction = track_reserve_node( request.train, node, direction );
 			break;
 		case TRACK_RESERVE_PUT:
-			direction = DIR_AHEAD;
-			if( node->type == NODE_BRANCH && switch_table[ SWID_TO_ARRAYID( node->id + 1 ) ] == 'C' ){
-				direction = DIR_CURVED;
-			}
 			if( track_reserved( request.train, node, direction ) == RESERVE_SUCCESS ){
 				node->edge[ direction ].train = 0;
 			}
@@ -164,6 +167,22 @@ int track_reserve_get_range( int tid, Train* train, int dist )
 	request.train = train;
 	request.node = train->check_point;
 	request.range_dir = dist;
+
+	status = track_reserve_request( tid, &request, &reply );
+	
+	return reply.direction;
+}
+
+int track_reserve_may_i( int tid, Train* train, track_node* node, int direction )
+{
+	Track_request request;
+	Track_reply reply;
+	int status;
+
+	request.type = TRACK_RESERVE_MAY_I;
+	request.train = train;
+	request.node = node;
+	request.range_dir = direction;
 
 	status = track_reserve_request( tid, &request, &reply );
 	
