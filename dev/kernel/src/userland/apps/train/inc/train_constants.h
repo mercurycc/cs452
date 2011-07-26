c0ade4a63b3fa2648ef4ff6911312d2985345c1c
gzwang <gzwang@uwaterloo.ca>
2011-07-25 21:42:56 -0400

    more accurate reservation positions

--- dev/kernel/src/userland/apps/train/inc/train_constants.h
+++ dev/kernel/src/userland/apps/train/inc/train_constants.h
@@ -1,11 +1,6 @@
 #ifndef _TRAIN_CONSTANTS_H_
 #define _TRAIN_CONSTANTS_H_
 
-/* 999 means not measured yet */
-#define TRAIN_HEAD_LENGTH( i ) ( i > 22 ? ( i == 23 ? 30 : 25 ) : ( i == 21 ? 20 : 999 ) )
-#define TRAIN_TAIL_LENGTH( i ) ( i > 22 ? ( i == 23 ? 140 : 145 ) : ( i == 21 ? 120 : 999 ) )
-
-#define TRAIN_SPEED_PRED_FACTOR( i ) ( i > 21 ? ( i == 23 ? 55 : 0 ) : ( i == 21 ? 22 : 0 ) )
 
 /* Measurements:
  * pickup = 50mm;
@@ -14,6 +9,7 @@
  * 24 : head = 25mm; tail = 145mm;
  */
 
+#define PICKUP_SIZE 50
 
 static inline int train_head_length( int id ){
 	switch ( id ) {
@@ -24,6 +20,7 @@ static inline int train_head_length( int id ){
 	case 24:
 		return 25;
 	default:
+		break;
 	}
 	return 0;
 }
@@ -37,8 +34,13 @@ static inline int train_tail_length( int id ){
 	case 24:
 		return 145;
 	default:
+		break;
 	}
 	return 0;
+}
+
+static inline int train_full_length( int id ){
+	return PICKUP_SIZE + train_head_length( id ) + train_tail_length( id );
 }
 
 #endif /* _TRAIN_CONSTANTS_H_ */
